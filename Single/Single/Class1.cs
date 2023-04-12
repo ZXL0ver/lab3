84747a133579b5ca9bb93442f1410dc01bf1878d
ZXL0ver <130565880+ZXL0ver@users.noreply.github.com>
2023-04-12 21:12:22 +0300

    Add files via upload

---
+++ Single/Single/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single
+{
+    public class Logger
+    {
+        private static Logger _instance = null;
+        private static readonly object _lock = new object();
+        private readonly string _logFilePath;
+
+        private Logger(string logFilePath)
+        {
+            _logFilePath = logFilePath;
+        }
+
+        public static Logger GetInstance(string logFilePath)
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Logger(logFilePath);
+                    }
+                }
+            }
+
+            return _instance;
+        }
+
+        public void Log(string message)
+        {
+            using (StreamWriter writer = File.AppendText(_logFilePath))
+            {
+                writer.WriteLine(message);
+            }
+        }
+    }
+}
---
+++ Single/Single/Program.cs
@@ -0,0 +1,5 @@
+﻿using Single;
+
+Logger logger = Logger.GetInstance(@"D:\HomeWork\Projects\Single\Single\bin\Debug\Singletone.txt");
+logger.Log("здійснено операцію 1");
+logger.Log("здійснено операцію 2");
